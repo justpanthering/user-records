fdf9a4c55ff5f4a3513a90dedc447dd43ad1f95b
justpanthering <ankitlakra@outlook.com>
2020-10-24 17:32:19 +0530

    Updates

---
+++ precache-manifest.fdfc3a148e28b2e8ec138ede540fae9b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0cbbb95fc3910d93e6190b7e9633331d",
+    "url": "/user-records/index.html"
+  },
+  {
+    "revision": "19168c49bfa5b31b3b2a",
+    "url": "/user-records/static/css/2.2a86faee.chunk.css"
+  },
+  {
+    "revision": "e0b88fb621dc19298cb5",
+    "url": "/user-records/static/css/main.a417a329.chunk.css"
+  },
+  {
+    "revision": "19168c49bfa5b31b3b2a",
+    "url": "/user-records/static/js/2.e370a409.chunk.js"
+  },
+  {
+    "revision": "44ba416b9a7217ca99c025d9f4dd1124",
+    "url": "/user-records/static/js/2.e370a409.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e0b88fb621dc19298cb5",
+    "url": "/user-records/static/js/main.679e8f43.chunk.js"
+  },
+  {
+    "revision": "3d5f06e0423bb556c266",
+    "url": "/user-records/static/js/runtime-main.26314dfc.js"
+  }
+]);
